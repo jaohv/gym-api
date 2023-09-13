d2e97d980c02eb79f7867c76f3402d0f2672c898
jaohv <joao.rmiranda@icloud.com>
2023-09-12 22:41:30 -0300

    feat: add new route create user

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({
-	host: '0.0.0.0 ',
+	host: '0.0.0.0',
 	port: env.PORT,
 }).then(() => {
-	console.log('dksadsak')
+	console.log('🚀 HTTP Server Running!')
 })
