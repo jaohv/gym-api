d8ecf867847dd2a1275e0fdc8ad1a7ed9b7efaf2
jaohv <joao.rmiranda@icloud.com>
2023-09-27 14:31:22 -0300

    feat: inversion depedencies

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,29 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { CreateUserService } from '@/services/createUserService'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+	const registerBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6)
+	})
+
+	const {name, email, password } = registerBodySchema.parse(request.body)
+
+	try {
+		const prismaUsersRespository = new PrismaUsersRepository
+		const createUserService =  new CreateUserService(prismaUsersRespository)
+
+		await createUserService.execute({
+			name,
+			email,
+			password
+		})
+	} catch (err) {
+		return reply.status(409).send()
+	}
+
+	return reply.status(201).send()
+}
