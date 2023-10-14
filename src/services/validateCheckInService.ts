65cf35ad94cd85ff8614913f36418c228266fe4d
jaohv <joao.rmiranda@icloud.com>
2023-10-14 00:35:04 -0300

    feat: create validated check in service

---
+++ src/services/validateCheckInService.ts
@@ -0,0 +1,33 @@
+import { ResourceNotFoundError } from './errors/resourceNotFoundError'
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface ValidateCheckInServiceRequest {
+	checkInId: string
+}
+
+interface ValidateCheckInServiceResponse {
+    checkIn: CheckIn
+}
+
+export class ValidateCheckInService {
+	constructor(
+        private checkInsRepository: CheckInsRepository,
+	) {}
+
+	async execute({checkInId}: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse>{
+		const checkIn = await this.checkInsRepository.findById(checkInId)
+
+		if(!checkIn) {
+			throw new ResourceNotFoundError()
+		}
+
+		checkIn.validated_at = new Date()
+
+		await this.checkInsRepository.save(checkIn)
+
+		return {
+			checkIn,
+		}
+	}
+}
